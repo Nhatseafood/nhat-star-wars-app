bcbb88b0ddf72ea07fe054dc5051ff73f61ec8c2
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-04-11 17:03:42 -0500

    test

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-
+const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
-
-app.set('views', path.join(__dirname, 'views'))
-
-var routes = require('./routes');
-
-app.use(express.static(path.join(__dirname, 'public')));
-//routes
-
-// home
-app.get('/', routes.home);
-
-
-//movie single
-app.get('/star_wars_episode/:episode_number?', routes.movie_single);
-
-//not found
-app.get('*', routes.notFound);
-
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-
-
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
