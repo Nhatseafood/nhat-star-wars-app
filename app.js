c0c3f75d01f88e334bdcc4b5d1f840003b624bba
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-04-11 17:01:43 -0500

    fix for heroku

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
-app.set('view engine', 'ejs');
+var path = require('path');
 
+
+const PORT = process.env.PORT || 5000
+
+
+app.set('views', path.join(__dirname, 'views'))
 
 var routes = require('./routes');
 
-var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 //routes
 
@@ -20,9 +24,7 @@ app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 app.get('*', routes.notFound);
 
 
-app.listen(3000, function() {
-    console.log("The application is running on localhost. 3000")
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
