025aa36ec22aa1479d2b33b80522a1f69906a4af
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-04-11 17:05:44 -0500

    reverting last fix

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var routes = require('./routes');
+
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+//routes
+
+app.set('view engine', 'ejs');
+
+// home
+app.get('/', routes.home);
+
+
+//movie single
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
+
+//not found
+app.get('*', routes.notFound);
+
+
+app.listen(3000, function() {
+    console.log("The application is running on localhost. 3000")
+});
+
